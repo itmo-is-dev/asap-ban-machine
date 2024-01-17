a16e8b67afe881411acc467e7c8be3b0177bcad7
ronimizy <ronimizy@outlook.com>
2024-01-17 20:31:25 +0300

    fix: syntax

--- src/Infrastructure/Itmo.Dev.Asap.BanMachine.Infrastructure.ML/Services/PythonRestoreBackgroundService.cs
+++ src/Infrastructure/Itmo.Dev.Asap.BanMachine.Infrastructure.ML/Services/PythonRestoreBackgroundService.cs
@@ -18,8 +18,8 @@ public class PythonRestoreBackgroundService : BackgroundService
     {
         string pypiToken = _options.PyPiToken;
 
-        await Cli.Wrap("python pip_install_privates --token ${pypiToken} requirements.txt")
-            .}WithValidation(CommandResultValidation.None)
+        await Cli.Wrap($"python pip_install_privates --token {pypiToken} requirements.txt")
+            .WithValidation(CommandResultValidation.None)
             .WithStandardOutputPipe(PipeTarget.Null)
             .WithStandardErrorPipe(PipeTarget.Null)
             .ExecuteAsync(stoppingToken);
