e541169ab9c92a94120fcc61b0b67d9d561ee381
ronimizy <ronimizy@outlook.com>
2024-01-17 17:59:52 +0300

    chore: added pypi restore

--- src/Infrastructure/Itmo.Dev.Asap.BanMachine.Infrastructure.ML/Extensions/ServiceCollectionExtensions.cs
+++ src/Infrastructure/Itmo.Dev.Asap.BanMachine.Infrastructure.ML/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
 using Itmo.Dev.Asap.BanMachine.Application.Abstractions.BanMachine;
+using Itmo.Dev.Asap.BanMachine.Infrastructure.ML.Options;
 using Itmo.Dev.Asap.BanMachine.Infrastructure.ML.Services;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -8,7 +9,11 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddInfrastructureMachineLearning(this IServiceCollection collection)
     {
+        collection.AddOptions<MachineLearningOptions>().BindConfiguration("Infrastructure:ML");
+
         collection.AddScoped<IBanMachineService, BanMachineService>();
+        collection.AddHostedService<PythonRestoreBackgroundService>();
+
         return collection;
     }
 }
---
+++ src/Infrastructure/Itmo.Dev.Asap.BanMachine.Infrastructure.ML/Options/MachineLearningOptions.cs
@@ -0,0 +1,6 @@
+namespace Itmo.Dev.Asap.BanMachine.Infrastructure.ML.Options;
+
+public class MachineLearningOptions
+{
+    public string PyPiToken { get; set; } = string.Empty;
+}
