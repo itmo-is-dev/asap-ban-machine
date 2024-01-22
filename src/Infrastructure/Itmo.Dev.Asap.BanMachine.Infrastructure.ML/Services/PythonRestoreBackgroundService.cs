597befd841b87b581e9e40121b166be57d8dd057
ronimizy <ronimizy@outlook.com>
2024-01-22 19:21:04 +0300

    chore: added restore logs

--- src/Infrastructure/Itmo.Dev.Asap.BanMachine.Infrastructure.ML/Services/PythonRestoreBackgroundService.cs
+++ src/Infrastructure/Itmo.Dev.Asap.BanMachine.Infrastructure.ML/Services/PythonRestoreBackgroundService.cs
@@ -1,16 +1,22 @@
 using CliWrap;
 using Itmo.Dev.Asap.BanMachine.Infrastructure.ML.Options;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using System.Text;
 
 namespace Itmo.Dev.Asap.BanMachine.Infrastructure.ML.Services;
 
 public class PythonRestoreBackgroundService : BackgroundService
 {
     private readonly MachineLearningOptions _options;
+    private readonly ILogger<PythonRestoreBackgroundService> _logger;
 
-    public PythonRestoreBackgroundService(IOptions<MachineLearningOptions> options)
+    public PythonRestoreBackgroundService(
+        IOptions<MachineLearningOptions> options,
+        ILogger<PythonRestoreBackgroundService> logger)
     {
+        _logger = logger;
         _options = options.Value;
     }
 
@@ -18,12 +24,29 @@ public class PythonRestoreBackgroundService : BackgroundService
     {
         string pypiToken = _options.PyPiToken;
 
+        var outputBuilder = new StringBuilder();
+        var errorBuilder = new StringBuilder();
+
         await Cli.Wrap("python3")
             .WithValidation(CommandResultValidation.None)
             .WithArguments($"pip_install_privates --token {pypiToken} requirements.txt")
             .WithWorkingDirectory(Directory.GetCurrentDirectory())
-            .WithStandardOutputPipe(PipeTarget.Null)
-            .WithStandardErrorPipe(PipeTarget.Null)
+            .WithStandardOutputPipe(PipeTarget.ToStringBuilder(outputBuilder))
+            .WithStandardErrorPipe(PipeTarget.ToStringBuilder(errorBuilder))
             .ExecuteAsync(stoppingToken);
+
+        if (outputBuilder.Length is not 0)
+        {
+            _logger.LogTrace(
+                "ML wrote to stdout: {Message}",
+                outputBuilder.ToString());
+        }
+
+        if (errorBuilder.Length is not 0)
+        {
+            _logger.LogTrace(
+                "ML wrote to stderr: {Message}",
+                errorBuilder.ToString());
+        }
     }
 }
