a80d44960e9de1ce0adcc9bcef26f8fbc002e435
Daniil Kazantsev <daniil.kazantsev@atom.team>
2024-01-25 17:52:27 +0300

    chore: restore script

---
+++ src/Infrastructure/restore.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ENV_NAME="asap-ban-machine"
+
+conda env list | awk '{print $1}' | grep -w $ENV_NAME
+if [ $? -eq 0 ]; then
+    echo "Environment $ENV_NAME exists, activating it"
+    conda activate $ENV_NAME
+else
+    echo "Environment $ENV_NAME does not exist, creating it"
+    conda create -n $ENV_NAME
+    conda activate $ENV_NAME
+fi
+
+while read requirement; do conda install --yes $requirement; done < requirements.txt
