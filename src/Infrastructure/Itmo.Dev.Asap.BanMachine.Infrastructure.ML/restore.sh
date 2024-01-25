315da4427c7de850a93acf163f1e830d008218d1
Daniil Kazantsev <daniil.kazantsev@atom.team>
2024-01-25 20:52:30 +0300

    fix: conda init

--- src/Infrastructure/Itmo.Dev.Asap.BanMachine.Infrastructure.ML/restore.sh
+++ src/Infrastructure/Itmo.Dev.Asap.BanMachine.Infrastructure.ML/restore.sh
@@ -5,9 +5,11 @@ ENV_NAME="asap-ban-machine"
 conda env list | awk '{print $1}' | grep -w $ENV_NAME
 if [ $? -eq 0 ]; then
     echo "Environment $ENV_NAME exists, activating it"
+    conda init
     conda activate $ENV_NAME
 else
     echo "Environment $ENV_NAME does not exist, creating it"
+    conda init
     conda create -n $ENV_NAME
     conda activate $ENV_NAME
 fi
