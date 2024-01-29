211ff78b125250c35386217c8377cbc8de1f4a9a
ronimizy <ronimizy@outlook.com>
2024-01-29 19:36:56 +0300

    chore: removed env creation

--- src/Infrastructure/Itmo.Dev.Asap.BanMachine.Infrastructure.ML/restore.sh
+++ src/Infrastructure/Itmo.Dev.Asap.BanMachine.Infrastructure.ML/restore.sh
@@ -1,17 +1,5 @@
 #!/bin/bash --login
 
-ENV_NAME="asap-ban-machine"
-
-conda env list | awk '{print $1}' | grep -w $ENV_NAME
-if [ $? -eq 0 ]; then
-    echo "Environment $ENV_NAME exists, activating it"
-    conda activate $ENV_NAME
-else
-    echo "Environment $ENV_NAME does not exist, creating it"
-    conda create -n $ENV_NAME
-    conda activate $ENV_NAME
-fi
-
 pip install /packages/asap_ban_machine_model-0.0.1-py3-none-any.whl
 
 #while read requirement; do conda install --yes $requirement; done < requirements.txt
