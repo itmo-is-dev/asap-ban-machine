a28bce3638d12adeb1e7ef7ad437bedca471d6fb
ronimizy <ronimizy@outlook.com>
2024-01-29 21:02:30 +0300

    fix: deps

--- src/Infrastructure/Itmo.Dev.Asap.BanMachine.Infrastructure.ML/restore.sh
+++ src/Infrastructure/Itmo.Dev.Asap.BanMachine.Infrastructure.ML/restore.sh
@@ -1,5 +1,6 @@
 #!/bin/bash --login
 
+pip install /packages/asap_ban_machine_model-0.0.1-py3-none-any.whl --force --no-deps
 pip install /packages/asap_ban_machine_model-0.0.1-py3-none-any.whl
 
 #while read requirement; do conda install --yes $requirement; done < requirements.txt
