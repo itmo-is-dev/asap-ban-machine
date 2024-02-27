ee19b263266925aef6cc118e00ac9b8d8312f5aa
ronimizy <ronimizy@outlook.com>
2024-02-28 01:48:43 +0300

    fix: migration

--- src/Infrastructure/Itmo.Dev.Asap.BanMachine.Infrastructure.Persistence/Migrations/1709073305_FixedCodeBlockType.cs
+++ src/Infrastructure/Itmo.Dev.Asap.BanMachine.Infrastructure.Persistence/Migrations/1709073305_FixedCodeBlockType.cs
@@ -11,18 +11,30 @@ public class FixedCodeBlockType : SqlMigration
     protected override string GetUpSql(IServiceProvider serviceProvider) =>
     """
     alter type code_block
-    rename attribute code_block_file_path to file_path,
-    rename attribute code_block_line_from to line_from,
-    rename attribute code_block_line_to to line_to,
+    rename attribute code_block_file_path to file_path;
+    
+    alter type code_block
+    rename attribute code_block_line_from to line_from;
+    
+    alter type code_block
+    rename attribute code_block_line_to to line_to;
+    
+    alter type code_block
     rename attribute code_block_content to content;
     """;
 
     protected override string GetDownSql(IServiceProvider serviceProvider) =>
     """
     alter type code_block
-    rename attribute file_path to code_block_file_path,
-    rename attribute line_from to code_block_line_from,
-    rename attribute line_to to code_block_line_to,
+    rename attribute file_path to code_block_file_path;
+    
+    alter type code_block
+    rename attribute line_from to code_block_line_from;
+    
+    alter type code_block
+    rename attribute line_to to code_block_line_to;
+    
+    alter type code_block
     rename attribute content to code_block_content;
     """;
 }
